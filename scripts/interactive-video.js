1de7174c62c260fd2f529b800f3ca29226ce9c0e
Frode Petterson <frode.petterson@amendor.com>
2014-02-19 14:52:54 +0100

    Revert "HFJ-175: Resizing of DragQuestion not working in IV fullscreen"
    This reverts commit 66f1389d3ead7727588495dfc2813404a733423a.

--- scripts/interactive-video.js
+++ scripts/interactive-video.js
@@ -733,7 +733,7 @@ H5P.InteractiveVideo = (function ($) {
       interactionInstance = new (H5P.classFromName(lib))(interaction.action.params, this.contentId);
       interactionInstance.attach($dialog);
 
-      if (lib === 'H5P.Summary' || lib === 'H5P.DragQuestion') {
+      if (lib === 'H5P.Summary') {
         interaction.bigDialog = true;
       }
     }
