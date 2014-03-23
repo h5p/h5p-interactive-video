6f36f10c2ece5078b70d021f2975b5da89e5c96d
Frode Petterson <frode.petterson@amendor.com>
2014-03-23 02:09:41 +0100

    HFJ-257: Fixed semi fullscreen in IV.

--- scripts/interactive-video.js
+++ scripts/interactive-video.js
@@ -470,17 +470,8 @@ H5P.InteractiveVideo = (function ($) {
   C.prototype.toggleFullScreen = function () {
     if (this.controls.$fullscreen.hasClass('h5p-exit')) {
       this.controls.$fullscreen.removeClass('h5p-exit').attr('title', this.l10n.fullscreen);
-      if (H5P.fullScreenBrowserPrefix === undefined) {        
-        // Click button to disable fullscreen
-        var button = $('#' + window.frameElement.id + '-wrapper', parent.document).children('.h5p-disable-fullscreen')[0];
-        if (button.dispatchEvent) {
-          var event = document.createEvent('MouseEvents');
-          event.initEvent('click', true, true);
-          button.dispatchEvent(event);
-        }
-        else if (button.fireEvent) {
-          button.fireEvent('onclick', document.createEventObject());
-        }
+      if (H5P.fullScreenBrowserPrefix === undefined) {
+        $('.h5p-disable-fullscreen').click();
       }
       else {
         if (H5P.fullScreenBrowserPrefix === '') {
@@ -499,7 +490,7 @@ H5P.InteractiveVideo = (function ($) {
       H5P.fullScreen(this.$container, this);
       if (H5P.fullScreenBrowserPrefix === undefined) {
         // Hide disable full screen button. We have our own!
-        $('#' + window.frameElement.id + '-wrapper', parent.document).children('.h5p-disable-fullscreen').hide();
+        $('.h5p-disable-fullscreen').hide();
       }
     }
   };
