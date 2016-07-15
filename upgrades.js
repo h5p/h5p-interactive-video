b9f4e2f9d60a2947ddc297b50dd97c8f9db8023c
Oliver Tacke <o.tacke@web.de>
2016-07-15 20:17:46 +0200

    added playbackRate key

--- upgrades.js
+++ upgrades.js
@@ -88,7 +88,7 @@ H5PUpgrades['H5P.InteractiveVideo'] = (function ($) {
         var i;
         parameters.l10n = {};
 
-        var keys = ['interaction', 'play', 'pause', 'mute', 'quality', 'unmute', 'fullscreen', 'exitFullscreen', 'summary', 'bookmarks', 'defaultAdaptivitySeekLabel'];
+        var keys = ['interaction', 'play', 'pause', 'mute', 'quality', 'unmute', 'fullscreen', 'exitFullscreen', 'summary', 'bookmarks', 'defaultAdaptivitySeekLabel', 'playbackRate'];
         for (i = 0; i < keys.length; i++) {
           var key = keys[i];
           if (parameters.hasOwnProperty(key)) {
