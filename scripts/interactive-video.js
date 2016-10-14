fdc3a9b2a44ee12b77c47a1ac54b1714fb2a8fa3
Timothy Lim <timothy_lim@outlook.com>
2016-10-14 10:49:36 +0200

    Update semantics [HFP-66]

--- scripts/interactive-video.js
+++ scripts/interactive-video.js
@@ -26,19 +26,18 @@ H5P.InteractiveVideo = (function ($, EventDispatcher, DragNBar, Interaction) {
       video: {},
       assets: {}
     }, params.interactiveVideo);
-    self.options.video.advancedSettings = self.options.video.advancedSettings || {};
-    self.options.video.advancedSettings.startScreenOptions = self.options.video.advancedSettings.startScreenOptions || {};
+    self.options.video.startScreenOptions = self.options.video.startScreenOptions || {};
 
     // Add default title
-    if (!self.options.video.advancedSettings.title) {
-      self.options.video.advancedSettings.title = 'Interactive Video';
+    if (!self.options.video.startScreenOptions.title) {
+      self.options.video.startScreenOptions.title = 'Interactive Video';
     }
 
     // Set default splash options
     self.startScreenOptions = $.extend({
       hideStartTitle: false,
       shortStartDescription: ''
-    }, self.options.video.advancedSettings.startScreenOptions);
+    }, self.options.video.startScreenOptions);
 
     // Set overrides for interactions
     if (params.override && (params.override.showSolutionButton || params.override.retryButton)) {
@@ -107,8 +106,8 @@ H5P.InteractiveVideo = (function ($, EventDispatcher, DragNBar, Interaction) {
 
     // set start time
     startAt = (self.previousState && self.previousState.progress) ? Math.floor(self.previousState.progress) : 0;
-    if (startAt === 0 && !!self.options.video.advancedSettings.startVideoAt) {
-      startAt = self.options.video.advancedSettings.startVideoAt;
+    if (startAt === 0 && !!self.options.video.startScreenOptions.startVideoAt) {
+      startAt = self.options.video.startScreenOptions.startVideoAt;
     }
 
     // Start up the video player
@@ -117,7 +116,7 @@ H5P.InteractiveVideo = (function ($, EventDispatcher, DragNBar, Interaction) {
       params: {
         sources: self.options.video.files,
         visuals: {
-          poster: self.options.video.advancedSettings.startScreenOptions.poster,
+          poster: self.options.video.startScreenOptions.poster,
           controls: self.justVideo,
           fit: false
         },
@@ -414,7 +413,7 @@ H5P.InteractiveVideo = (function ($, EventDispatcher, DragNBar, Interaction) {
               '<div class="h5p-splash-main-outer">' +
                 '<div class="h5p-splash-main-inner">' +
                   '<div class="h5p-splash-play-icon"></div>' +
-                  '<div class="h5p-splash-title">' + this.options.video.advancedSettings.title + '</div>' +
+                  '<div class="h5p-splash-title">' + this.options.video.startScreenOptions.title + '</div>' +
                 '</div>' +
               '</div>' +
             '</div>' +
@@ -1783,7 +1782,7 @@ H5P.InteractiveVideo = (function ($, EventDispatcher, DragNBar, Interaction) {
    * @returns {string}
    */
   InteractiveVideo.prototype.getTitle = function() {
-    return H5P.createTitle(this.options.video.advancedSettings.title);
+    return H5P.createTitle(this.options.video.startScreenOptions.title);
   };
 
   /**
@@ -1854,12 +1853,12 @@ H5P.InteractiveVideo = (function ($, EventDispatcher, DragNBar, Interaction) {
     }
 
     // Adding info from copyright field
-    if (self.options.video.advancedSettings.copyright !== undefined) {
-      info.addMedia(self.options.video.advancedSettings.copyright);
+    if (self.options.video.startScreenOptions.copyright !== undefined) {
+      info.addMedia(self.options.video.startScreenOptions.copyright);
     }
 
     // Adding copyrights for poster
-    var poster = self.options.video.advancedSettings.startScreenOptions.poster;
+    var poster = self.options.video.startScreenOptions.poster;
     if (poster && poster.copyright !== undefined) {
       var image = new H5P.MediaCopyright(poster.copyright, self.l10n);
       var imgSource = H5P.getPath(poster.path, self.contentId);
