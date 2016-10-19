12f786b1cfab44683477067142900d83ea6bff1a
Frode Petterson <frode.andre.petterson@gmail.com>
2016-10-19 16:04:10 +0200

    Fixes video never loading in Chrome on iPad
    HFP-72

--- scripts/interactive-video.js
+++ scripts/interactive-video.js
@@ -149,8 +149,8 @@ H5P.InteractiveVideo = (function ($, EventDispatcher, DragNBar, Interaction) {
     var firstPlay = true;
     self.video.on('stateChange', function (event) {
 
-      if (!self.controls) {
-        // Add controls if they're missing
+      if (!self.controls && self.oneSecondInPercentage !== undefined) {
+        // Add controls if they're missing and 'loaded' has happened
         self.addControls();
         self.trigger('resize');
       }
@@ -208,6 +208,7 @@ H5P.InteractiveVideo = (function ($, EventDispatcher, DragNBar, Interaction) {
 
           // Make sure we track buffering of the video.
           self.startUpdatingBufferBar();
+
           break;
       }
     });
@@ -376,11 +377,19 @@ H5P.InteractiveVideo = (function ($, EventDispatcher, DragNBar, Interaction) {
       });
     }
 
-    if (this.currentState === InteractiveVideo.LOADED) {
-      if (!this.video.pressToPlay) {
+    if (!this.video.pressToPlay) {
+      if (this.currentState === InteractiveVideo.LOADED) {
+        // Add all controls
         this.addControls();
       }
+      else {
+        // Add splash to allow start playing before video load
+        // (play may be needed to trigger load incase preloaded="none" is default)
+        this.addSplash();
+      }
     }
+
+
     this.currentState = InteractiveVideo.ATTACHED;
   };
 
@@ -401,7 +410,7 @@ H5P.InteractiveVideo = (function ($, EventDispatcher, DragNBar, Interaction) {
    */
   InteractiveVideo.prototype.addSplash = function () {
     var that = this;
-    if (this.editor !== undefined || this.video.pressToPlay || !this.video.play) {
+    if (this.editor !== undefined || this.video.pressToPlay || !this.video.play || this.$splash) {
       return;
     }
 
