669f5014f07c64de0663e86f679c403395f9255c
Paal Joergensen <pal.jorgensen@gmail.com>
2017-08-09 13:03:22 +0200

    Removed æsj

--- src/scripts/selector-control.js
+++ src/scripts/selector-control.js
@@ -60,9 +60,7 @@ const SelectorControl = function (name, options, selectedOption, menuItemType, l
    */
   var toggle = function () {
     var isExpanded = self.control.getAttribute('aria-expanded') === 'true';
-    /* jshint -W030 */
     isExpanded ? hide() : show();
-    /* jshint +W030 */
   };
 
   /**
