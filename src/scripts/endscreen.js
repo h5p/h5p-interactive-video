9728304e7d3155897fe92c07985c1e36eb1a2293
Lars-Magnus Underhaug <lars-magnus.underhaug@joubel.com>
2023-10-12 09:37:55 +0200

    JI-5566 Check if submit button is enabled

--- src/scripts/endscreen.js
+++ src/scripts/endscreen.js
@@ -242,7 +242,9 @@ class Endscreen extends H5P.EventDispatcher {
       .filter(interaction => interaction.getProgress() !== undefined)
       .sort((a, b) => a.getDuration().from - b.getDuration().from);
 
-    this.$submitButtonContainer?.addClass(ENDSCREEN_STYLE_BUTTON_HIDDEN);
+    if (this.isSubmitButtonEnabled) {
+      this.$submitButtonContainer?.addClass(ENDSCREEN_STYLE_BUTTON_HIDDEN);
+    }
     this.$endscreenBottomTable.empty();
 
     // No chaining because we need the variable later
